a5a89df05818237ca7a6ad875fa0af04548212d5
alperenunal <alpernunl@gmail.com>
2023-06-18 12:39:00 +0300

    added helper methods

--- src/main/java/com/the/menu/repositories/CategoryRepository.java
+++ src/main/java/com/the/menu/repositories/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.the.menu.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.the.menu.models.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
+    Optional<Category> findByName(String name);
 }
--- src/main/java/com/the/menu/repositories/ItemRepository.java
+++ src/main/java/com/the/menu/repositories/ItemRepository.java
@@ -1,6 +1,7 @@
 package com.the.menu.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,6 @@ import com.the.menu.models.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
     List<Item> findAllByCategory(Category category);
+
+    Optional<Item> findByName(String name);
 }
